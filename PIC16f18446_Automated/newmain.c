252f5ecd587111738ed7a4ec8cdbcaf662a332dd
sridhar vumma <50115187+sridatascience@users.noreply.github.com>
2023-03-10 18:09:55 +0530

    changed to volatile  wherever needed

--- PIC16f18446_Automated/newmain.c
+++ PIC16f18446_Automated/newmain.c
@@ -9,18 +9,18 @@
 // CONFIG5
 #pragma config WDTE = OFF        // WDT operating mode (WDT enabled regardless of sleep; SWDTEN is ignored)
 #include <xc.h>
-uint8_t m =10;
-uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
+volatile uint8_t m =10;
+volatile uint8_t add(uint8_t n, uint8_t o, uint8_t p ){
    return n+o+p;
 }
 void main() {
-   uint8_t a = 0x05;
+   volatile uint8_t a = 0x05;
    TMR1H = a;
-   uint8_t c = 0x1F;
+   volatile uint8_t c = 0x1F;
    TMR1L = c;
-   uint8_t b =0x08;
+   volatile uint8_t b =0x08;
    T1CON = b;
-   uint8_t t = add(a,b,c);
+   volatile uint8_t t = add(a,b,c);
    T2CON = t;
    return;
 }
